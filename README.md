89b026691002cc63db3239a2790bb1042c393bb3
NHQ <jonathon@nationalheadquarters.org>
2013-06-26 13:34:52 -0700

    seems to working all right

---
+++ README.md
@@ -0,0 +1,59 @@
+# Nota Bene
+
+Some of Node.j's file system APIs implemented in the browser with the File System API. Currently, only asynchronous methods are provided, per the File System API (sync is in the spec but not supported yet). All methods copy Node's FS module.
+
+__Nota Bene__ uses [through](https://github.com/dominictarr/through) to expose read and write streams.
+
+## Methods
+* createWriteStream
+* createReadStream
+* writeFile
+* write
+* unlink
+* rename (aka mv)
+* read
+* readdir
+* mkdir
+* rmdir
+* stat
+
+There is also a helper method for the File System API.
+* setStorage(type, size) 
+
+Calling this method with reinit he fs for you in private. The defaults are 
+```js
+type = window.PERMANENT // otherwise window.TEMPORARY
+size = 1024 * 1024 * 1024 // in bytes
+```
+
+## usage
+```
+npm install nota-bene
+```
+Then use it as you would the FS module
+```js
+var fs = require('nota-bene')
+fs.createWriteStream('/tmp/audio.raw')
+fs.write(data)
+```
+
+## Tests
+
+Currently the test is an example with some logging.
+It can be run like so:
+
+First install browserify and opa
+```
+npm install -g browserify opa
+```
+Then:
+```
+git clone https://github.com/NHQ/nbfs
+cd nbfs
+opa -n -e test.js
+```
+you can pass a port number:
+```
+opa -n -e test.js -p 5000
+```
+
--- test.js
+++ test.js
@@ -1,44 +1,67 @@
-var getFileSystem = require('./getFileSystem');
-var fs = null;
+var fs = require('./nota-bene');
+
 var buf = new Float32Array(1024 * 1024);
 for(var x = 0; x < buf.length; x++){
 	buf[x] = Math.sin((x / 1024 * 1024) * Math.PI * 2)
 }
-getFileSystem(function(err, fs){
-	if(err) console.log(err);
+fs.mkdir('/tmp', function(err){
+	if(err) console.log(err)
 	else{
-		
-		var ws = fs.createWriteStream('tempeth3')
-		for(var y = 0; y < 5; y++){
-			ws.write(new Blob([buf]))			
-		}
-		
-		setTimeout(function(){
-			
-			var rs = fs.createReadStream('tempeth3')
-			rs.on('data', function(data){
+		fs.writeFile('/tmp/pipeTest', buf, function(err){
+			if(err) console.log(err)
+			var ws = fs.createWriteStream('/tmp/pip')
+			var rs = fs.createReadStream('/tmp/pipeTest')
+			rs.pipe(ws)
+			ws.on('data', function(data){
 				console.log(data)
 			})
-/*					
-			fs.root.getFile('tempeth3', {}, function(fileEntry) {
-
-			  fileEntry.file(function(file) {
-			     var reader = new FileReader();
-
-			     reader.onloadend = function(e) {
-
-			       console.log(this.result);
-
-			     };
-
-			     reader.readAsText(file);
-			  }, errorHandler);
-
-			}, errorHandler);
-*/
-		}, 1000)
+			setTimeout(function(){
+				fs.readFile('/tmp/pip', 'arraybuffer', function(err, file){
+					console.log('pipetest', err, file)
+				})
+			}, 1000)
+		})		
 	}
 })
+
+
+var ws = fs.createWriteStream('tempeth3')
+ws.on('error', function(err){console.log(err)})
+
+for(var y = 0; y < 5; y++){
+	ws.write(new Blob([buf]))
+	console.log('write!')			
+}
+
+fs.write('testbed', 'a string is the thing', 0, 8, null, function(err, written, buf){
+	console.log(err, written, buf)
+	fs.readFile('testbed', 'utf8', function(err, file){
+		console.log(file) // should be "a string"
+		fs.rename("testbed", '/tmp/test.txt', function(err){
+			console.log(err || 'success')
+		})
+	})
+})
+
+fs.writeFile('testbed2', 'a string is the thing', function(err, written, buf){
+	console.log(err, written, buf)
+	fs.readFile('testbed2', 'utf8', function(err, file){
+		console.log(file) // should be "a string is the thing"
+		fs.unlink('testbed2', function(err){
+			console.log(err) // null
+		})
+	})
+})
+
+
+setTimeout(function(){
+	
+	var rs = fs.createReadStream('tempeth3')
+	rs.on('data', function(data){
+		console.log('read!')
+	})
+
+}, 1000)
 
 
 function errorHandler(err){
